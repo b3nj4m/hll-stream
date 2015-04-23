a057da19e60a53969023cdef16841a23b486547d
Ben Robbins <ben.robbins@gmail.com>
2015-04-23 17:43:05 -0500

    tweaks

--- index.js
+++ index.js
@@ -10,16 +10,14 @@ var MAX_INT_BYTES = Math.ceil(MAX_INT_BITS / 8);
 function HLL(precision, hashType) {
   Stream.Writable.call(this);
 
-  this.precision = Math.min(16, Math.max(8, precision || 8));
-  //TODO bit precision
-  this.bytePrecision = Math.ceil(this.precision / 8);
+  this.precision = Math.min(16, Math.max(4, precision || 4));
   this.hashType = hashType || 'sha1';
-  this.logSize = Math.pow(2, this.precision);
-  this.log = new Array(this.logSize);
+  this.registersSize = 1 << this.precision;
+  this.registers = new Array(this.registersSize);
   this.alpha = (this.alphaTable[this.precision] || this.alphaTable.default)(this.precision);
 
-  for (var i = 0; i < this.log.length; i++) {
-    this.log[i] = 0;
+  for (var i = 0; i < this.registers.length; i++) {
+    this.registers[i] = 0;
   }
 }
 
@@ -40,13 +38,13 @@ HLL.prototype.write = function(chunk, enc, next) {
     chunk = new Buffer(chunk);
   }
 
-  var hash = crypto.createHash(this.hashType).update(chunk).digest();
-  var idx = parseInt(hash.slice(0, this.bytePrecision).toString('hex'), 16);
-  var estimator = parseInt(hash.slice(this.bytePrecision, MAX_INT_BYTES).toString('hex'), 16);
+  var hash = parseInt(crypto.createHash(this.hashType).update(chunk).digest().slice(0, MAX_INT_BYTES).toString('hex'), 16);
+  var idx = hash & (this.registersSize - 1);
+  var estimator = hash >> this.precision;
 
   var estimatorBits = MAX_INT_BITS - this.precision;
   var i;
-  var mask = Math.pow(2, estimatorBits);
+  var mask = 1 << estimatorBits;
 
   for (i = 0; i < estimatorBits; i++) {
     if ((estimator & mask) !== 0) {
@@ -55,7 +53,7 @@ HLL.prototype.write = function(chunk, enc, next) {
     mask = mask >> 1;
   }
 
-  this.log[idx] = Math.max(this.log[idx], i + 1);
+  this.registers[idx] = Math.max(this.registers[idx], i + 1);
 
   if (next) {
     next();
@@ -64,10 +62,10 @@ HLL.prototype.write = function(chunk, enc, next) {
   return true;
 };
 
-HLL.prototype.zeros = function() {
+HLL.prototype.numZeros = function() {
   var zeros = 0;
-  for (var i = 0; i < this.log.length; i++) {
-    if (this.log[i] === 0) {
+  for (var i = 0; i < this.registers.length; i++) {
+    if (this.registers[i] === 0) {
       zeros++;
     }
   }
@@ -76,21 +74,21 @@ HLL.prototype.zeros = function() {
 
 HLL.prototype.cardinality = function() {
   var sum = 0;
-  for (i = 0; i < this.log.length; i++) {
-    sum += Math.pow(2, -this.log[i]);
+  for (var i = 0; i < this.registers.length; i++) {
+    sum += Math.pow(2, -this.registers[i]);
   }
 
-  var estimate = (this.alpha * this.logSize * this.logSize) / sum;
+  var estimate = (this.alpha * this.registersSize * this.registersSize) / sum;
 
-  if (estimate <= 5 * this.logSize) {
+  if (estimate <= 5 * this.registersSize) {
     return estimate - this.estimateBias(estimate);
   }
 
-  var zeros = this.zeros();
+  var zeros = this.numZeros();
   var thresholdMetric;
 
   if (zeros > 0) {
-    thresholdMetric = this.logSize * Math.log(this.logSize / zeros);
+    thresholdMetric = this.registersSize * Math.log(this.registersSize / zeros);
   }
   else {
     thresholdMetric = estimate;
@@ -106,13 +104,16 @@ HLL.prototype.cardinality = function() {
 HLL.prototype.estimateBias = function(estimate) {
   var biasVector = biasData[this.precision - 4];
   
-  var closestEstimates = rawEstimateData[this.precision - 4].map(function(estimateValue, i) {
-    return {val: Math.pow(estimate - estimateValue, 2), idx: i};
-  });
+  var closestEstimates = [];
+  var estimateVector = rawEstimateData[this.precision - 4];
+
+  for (var i = 0; i < estimateVector.length; i++) {
+    closestEstimates.push({val: Math.pow(estimate - estimateVector[i], 2), idx: i});
+  }
 
   closestEstimates = closestEstimates.sort(function(a, b) {
     return a.val - b.val;
-  }).slice(0, 6);
+  }).slice(0, 5);
 
   var bias = 0;
   for (var i = 0; i < closestEstimates.length; i++) {
